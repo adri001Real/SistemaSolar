a640c725630717bc1edf3c7f1a3be092ee6fa431
Adriana Real <lucia05.real@gmail.com>
2024-06-06 23:05:59 -0600

    librerias necesarias listas!

---
+++ Main.cpp
@@ -0,0 +1,33 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stb_image.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H   
+
+#include "Shader.h"
+#include "Sphere.h"
+#include "Camera.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <wtypes.h>
+#include <ctime>
+#include <filesystem>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define TAU (M_PI * 2.0)
+
+int main() {
+
+	return 0;
+}
